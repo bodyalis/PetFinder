e6b776abe978e0fb6ee002eb45fa9996e44b66fb
Bogdan <listopad.bogdan@bk.ru>
2025-02-18 22:33:49 +0300

    Добавил метод AddHandlersFromAssembly

--- backend/src/PetFinder.Application/Extensions/HandlerExtensions.cs
+++ backend/src/PetFinder.Application/Extensions/HandlerExtensions.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Microsoft.Extensions.DependencyInjection;
 using PetFinder.Application.Features.Shared.Interfaces;
 
@@ -7,14 +8,42 @@ internal static class HandlerExtensions
 {
     public static IServiceCollection AddHandlersFromAssembly(this IServiceCollection services)
     {
-        var interfaceType = typeof(IHandler);
-        var types = typeof(IHandler).Assembly.GetTypes()
-            .Where(p => interfaceType.IsAssignableFrom(p) && !p.IsInterface)
+        var commandHandlerTypes = typeof(ICommandHandler<>).Assembly.GetTypes()
+            .Where(t => !t.IsAbstract && !t.IsInterface)
+            .SelectMany(t => t.GetInterfaces(), (t, i) => new { HandlerType = t, InterfaceType = i })
+            .Where(x => x.InterfaceType.IsGenericType &&
+                        x.InterfaceType.GetGenericTypeDefinition() == typeof(ICommandHandler<>))
             .ToList();
 
-        foreach (var type in types)
+        foreach (var handler in commandHandlerTypes)
         {
-            services.AddScoped(type);
+            services.AddScoped(handler.InterfaceType, handler.HandlerType);
+        }
+
+        // Регистрация всех ICommandHandlerWithResponse<in T, TResponse>
+        var commandHandlerWithResponseTypes = typeof(ICommandHandlerWithResponse<,>).Assembly.GetTypes()
+            .Where(t => !t.IsAbstract && !t.IsInterface)
+            .SelectMany(t => t.GetInterfaces(), (t, i) => new { HandlerType = t, InterfaceType = i })
+            .Where(x => x.InterfaceType.IsGenericType &&
+                        x.InterfaceType.GetGenericTypeDefinition() == typeof(ICommandHandlerWithResponse<,>))
+            .ToList();
+
+        foreach (var handler in commandHandlerWithResponseTypes)
+        {
+            services.AddScoped(handler.InterfaceType, handler.HandlerType);
+        }
+        
+        // Регистрация всех IQueryHandler<in T, TResponse>
+        var queryHandlerTypes = typeof(IQueryHandler<,>).Assembly.GetTypes()
+            .Where(t => !t.IsAbstract && !t.IsInterface)
+            .SelectMany(t => t.GetInterfaces(), (t, i) => new { HandlerType = t, InterfaceType = i })
+            .Where(x => x.InterfaceType.IsGenericType &&
+                        x.InterfaceType.GetGenericTypeDefinition() == typeof(IQueryHandler<,>))
+            .ToList();
+
+        foreach (var handler in queryHandlerTypes)
+        {
+            services.AddScoped(handler.InterfaceType, handler.HandlerType);
         }
 
         return services;
